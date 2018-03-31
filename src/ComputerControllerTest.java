9d84502e8b3132879d1a2c0000b6770fa661a7be
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-03-31 13:13:40 -0400

    Clean up ComputerController again

--- src/ComputerControllerTest.java
+++ src/ComputerControllerTest.java
@@ -2,10 +2,11 @@ import java.awt.*;
 
 public class ComputerControllerTest {
     public static void main(String[] args){
-//        int keyCodes[] = {65, 77, 65, 90, 79, 78};
-        ComputerController robot = new ComputerController();
+        int keyCodes[] = {65, 77, 65, 90, 79, 78};
 
-        int keyCodes[] = {39, 39, 39, 39, 39, 39};
+        ComputerController robot = new ComputerController();
+//        int keyCodes[] = {39, 39, 39, 39, 39, 39};
+        
         for(int i=0; i< keyCodes.length; i++) {
 
             try {
