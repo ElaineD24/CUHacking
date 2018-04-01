4e679118674e787807c2b203840dd2cd6a434d09
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-03-31 23:38:06 -0400

    kj

--- src/ComputerController.java
+++ src/ComputerController.java
@@ -1,5 +1,8 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.event.InputEvent;
+import java.security.Key;
+import java.util.HashMap;
 
 public class ComputerController {
 
@@ -7,10 +10,15 @@ public class ComputerController {
     //private static Point p;
     private int initialX;
     private int initialY;
-    private final int AUTO_DELAY = 100;
+    private final int AUTO_DELAY = 1000;
+
+    private Boolean shiftHold;
+    private final int ShiftKeyCode = 16;
+
+    private static HashMap<Integer, Integer> SpecialKeys;
 
     public ComputerController(){
-
+        //System.out.println("creating new");
         try {
             this.robot = new Robot();
 
@@ -19,6 +27,27 @@ public class ComputerController {
         } catch (AWTException e) {
             e.printStackTrace();
         }
+
+        shiftHold = false;
+
+        SpecialKeys = new HashMap<>();
+        //SpecialKeys.put(8, KeyEvent.VK_BACK_SPACE);
+        SpecialKeys.put(9, KeyEvent.VK_TAB);
+        SpecialKeys.put(13, KeyEvent.VK_ENTER);
+        SpecialKeys.put(27, KeyEvent.VK_ESCAPE);
+        SpecialKeys.put(32, KeyEvent.VK_SPACE);
+        SpecialKeys.put(46, KeyEvent.VK_DELETE);
+        SpecialKeys.put(186, KeyEvent.VK_SEMICOLON);
+        SpecialKeys.put(187, KeyEvent.VK_EQUALS);
+        SpecialKeys.put(188, KeyEvent.VK_COMMA);
+        SpecialKeys.put(189, KeyEvent.VK_MINUS);
+        SpecialKeys.put(190, KeyEvent.VK_PERIOD);
+        SpecialKeys.put(191, KeyEvent.VK_SLASH);
+        SpecialKeys.put(192, KeyEvent.VK_BACK_QUOTE);
+        SpecialKeys.put(219, KeyEvent.VK_OPEN_BRACKET);
+        SpecialKeys.put(220, KeyEvent.VK_BACK_SLASH);
+        SpecialKeys.put(221, KeyEvent.VK_CLOSE_BRACKET);
+        SpecialKeys.put(222, KeyEvent.VK_QUOTE);
     }
 
     public void init(float x, float y){
@@ -57,10 +86,50 @@ public class ComputerController {
         this.robot.mouseWheel(-1);
     }
 
-    public static void keyPress(int key) throws AWTException {
+    public void toggleShift() throws AWTException {
+        /*try {
+            Thread.sleep(AUTO_DELAY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         Robot robot = new Robot();
-        robot.keyPress(key);
-        robot.keyRelease(key);
+
+        //shift key code: 16
+        if (shiftHold) {
+            System.out.println("Releasing Shift");
+            robot.keyRelease(ShiftKeyCode);
+            shiftHold = false;
+        }
+        else {
+            System.out.println("Holding Shift");
+            robot.keyPress(ShiftKeyCode);
+            shiftHold = true;
+        }
+    }
+
+    public void keyPress(int key) throws AWTException {
+        try {
+            Thread.sleep(AUTO_DELAY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Key Code: " + key);
+
+        Robot robot = new Robot();
+
+        if (key == ShiftKeyCode) {
+            toggleShift();
+        }
+        else if (SpecialKeys.containsKey(key)) {
+            robot.keyPress(SpecialKeys.get(key));
+            robot.keyRelease(SpecialKeys.get(key));
+        }
+        else {
+            robot.keyPress(key);
+            robot.keyRelease(key);
+        }
+
     }
 
     public void mouseMovement(float differenceX, float differenceY){
--- src/ComputerControllerTest.java
+++ src/ComputerControllerTest.java
@@ -1,5 +1,23 @@
 import java.awt.*;
 
+import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.net.Socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.net.URL;
+
 public class ComputerControllerTest {
     public static void main(String[] args){
         int keyCodes[] = {65, 77, 65, 90, 79, 78};
